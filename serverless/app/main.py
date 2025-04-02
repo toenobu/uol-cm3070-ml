4bfca3b6ff48471edba997d6050ad5b912ea1817
toenobu <me@toenobu.name>
2025-04-02 21:36:32 +0900

    fix: Add HTTPS protocol to API endpoint URL

--- serverless/app/main.py
+++ serverless/app/main.py
@@ -171,7 +171,7 @@ def handler(event, context):
     if api_endpoint:
         try:
             response = requests.post(
-                api_endpoint,
+                f"https://{api_endpoint}",
                 json=body_data,
                 headers={'Content-Type': 'application/json', 'x-tomato': x_tomato_header_value}
             )
